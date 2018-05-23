99c3b1ca98137bd57b4f465363163ec80b84379a
Jan Rádl <radlj@rferl.org>
2018-05-23 11:17:24 +0200

    Changes in .gitignore

--- gruntfile.js
+++ gruntfile.js
@@ -4,10 +4,35 @@
 
 module.exports = function(grunt) {
     grunt.initConfig({
-        uglify: {
+        sass: {
             apolloFramework: {
                 files: {
+                    'dist/css/apollo-framework-2.0-launch.min.css': 'src/sass/launch.scss',
+                    'dist/css/apollo-framework-2.0-ct-cyrillic-light.min.css': 'src/sass/main-ct-cyrillic-light.scss',
+                    'dist/css/apollo-framework-2.0-ct-cyrillic-dark.min.css': 'src/sass/main-ct-cyrillic-dark.scss',
+                    'dist/css/apollo-framework-2.0-rfe-arabic-light.min.css': 'src/sass/main-rfe-arabic-light.scss',
+                    'dist/css/apollo-framework-2.0-rfe-arabic-dark.min.css': 'src/sass/main-rfe-arabic-dark.scss',
+                    'dist/css/apollo-framework-2.0-rfe-armenian-light.min.css': 'src/sass/main-rfe-armenian-light.scss',
+                    'dist/css/apollo-framework-2.0-rfe-armenian-dark.min.css': 'src/sass/main-rfe-armenian-dark.scss',
+                    'dist/css/apollo-framework-2.0-rfe-georgian-light.min.css': 'src/sass/main-rfe-georgian-light.scss',
+                    'dist/css/apollo-framework-2.0-rfe-georgian-dark.min.css': 'src/sass/main-rfe-georgian-dark.scss',
+                    'dist/css/apollo-framework-2.0-rfe-latin-cyrillic-light.min.css': 'src/sass/main-rfe-latin-cyrillic-light.scss',
+                    'dist/css/apollo-framework-2.0-rfe-latin-cyrillic-dark.min.css': 'src/sass/main-rfe-latin-cyrillic-dark.scss'
+
+                }
+            }
+        },
+        uglify: {
+            apolloFramework: {
+                options: {
+                    mangle: false,
+                    compress: {
+                        drop_console: false
+                    }
+                },
+                files: {
                     'dist/js/apollo-framework-2.0.min.js': [
+                        'src/js/configs/_configSVG.js',
                         'src/js/configs/_configSites.js',
                         'src/js/configs/_configPaths.js',
                         'src/js/configs/_configSetupWizard.js',
@@ -18,78 +43,41 @@ module.exports = function(grunt) {
                         'src/js/_renderPage.js',
                         'src/js/_initPage.js',
                         'src/js/_initImages.js',
-                        'src/js/vendors/plyr/plyr.js',
-                        'src/js/_initVideos.js',
                         'src/js/vendors/photoswipe/photoswipe-ui-default.min.js',
-                        'src/js/vendors/photoswipe/photoswipe.min.js'
+                        'src/js/vendors/photoswipe/photoswipe.min.js',
+                        'src/js/vendors/plyr/plyr.js',
+                        'src/js/_initVideos.js'
                     ]
                 }
             }
         },
-        sass: {
-            apolloFramework: {
-                files: {
-                    'dist/css/launch.css': 'src/sass/launch.scss',
-                    /*'dist/css/apollo-framework-2.0.css': 'src/sass/main.scss',*/
-                    'dist/css/apollo-framework-rfe-latin-light-2.0.css': 'src/sass/main-rfe-latin-light.scss',
-                    'dist/css/apollo-framework-rfe-latin-dark-2.0.css': 'src/sass/main-rfe-latin-dark.scss',
-                    'dist/css/apollo-framework-rfe-arabic-light-2.0.css': 'src/sass/main-rfe-arabic-light.scss',
-                    'dist/css/apollo-framework-rfe-arabic-dark-2.0.css': 'src/sass/main-rfe-arabic-dark.scss'
-                }
-            }
-        },
-        cssmin: {
-            apolloFramework: {
-                files: {
-                    'dist/css/launch.min.css': 'dist/css/launch.css',
-                    'dist/css/apollo-framework-rfe-latin-light-2.0.min.css': [
-                        'dist/css/apollo-framework-rfe-latin-light-2.0.css',
-                        'src/sass/vendors/photoswipe/default-skin.css',
-                        'src/sass/vendors/photoswipe/photoswipe.css',
-                        'src/sass/vendors/plyr/plyrApollo.css'
-                    ],
-                    'dist/css/apollo-framework-rfe-latin-dark-2.0.min.css': [
-                        'dist/css/apollo-framework-rfe-latin-dark-2.0.css',
-                        'src/sass/vendors/photoswipe/default-skin.css',
-                        'src/sass/vendors/photoswipe/photoswipe.css',
-                        'src/sass/vendors/plyr/plyrApollo.css'
-                    ],
-                    'dist/css/launch.min.css': 'dist/css/launch.css',
-                    'dist/css/apollo-framework-rfe-arabic-light-2.0.min.css': [
-                        'dist/css/apollo-framework-rfe-arabic-light-2.0.css',
-                        'src/sass/vendors/photoswipe/default-skin.css',
-                        'src/sass/vendors/photoswipe/photoswipe.css',
-                        'src/sass/vendors/plyr/plyrApollo.css'
-                    ],
-                    'dist/css/apollo-framework-rfe-arabic-dark-2.0.min.css': [
-                        'dist/css/apollo-framework-rfe-arabic-dark-2.0.css',
-                        'src/sass/vendors/photoswipe/default-skin.css',
-                        'src/sass/vendors/photoswipe/photoswipe.css',
-                        'src/sass/vendors/plyr/plyrApollo.css'
-                    ]
-                }
+        postcss: {
+            options: {
+                map: true,
+                processors: [
+                    require('autoprefixer')({ browsers: 'last 2 versions' }),
+                    require('cssnano')()
+                ]
+            },
+            dist: {
+                src: 'dist/css/*.css'
             }
         },
         watch: {
-            uglify: {
-                files: 'src/js/*.*',
-                tasks: ['uglify'],
-            },
             sass: {
-                files: ['src/sass/*.scss', 'src/sass/launch/*.scss', 'src/sass/cultures/*.scss', 'src/sass/scaffolding/*.scss', 'src/sass/elements/*.scss', 'src/sass/themes/*.scss'],
-                tasks: ['sass'],
+                files: ['src/sass/**/*.scss'],
+                tasks: ['sass', 'postcss']
             },
-            cssmin: {
-                files: ['dist/css/apollo-framework-2.0.css', 'dist/css/launch.css'],
-                tasks: ['cssmin'],
+            uglify: {
+                files: ['src/js/*.js', 'src/js/configs/*.js', 'src/js/vendors/photoswipe/*.js', 'src/js/vendors/plyr/*.js'],
+                tasks: ['uglify']
             }
         },
     });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-postcss');
     grunt.loadNpmTasks('grunt-contrib-watch');
-
-    grunt.registerTask('default', ["watch"]);
+    grunt.registerTask('default', ['sass', 'uglify', 'postcss', 'watch']);
 };
